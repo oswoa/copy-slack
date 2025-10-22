5b5ce04bc80a015600170569d3524230b8acb420
osw <ows.oao@gmail.com>
2025-10-23 04:55:41 +0900

    トーストにアニメーションを追加

--- src/app/common/components/Toast.tsx
+++ src/app/common/components/Toast.tsx
@@ -1,21 +1,28 @@
-import { Alert, AlertColor, Snackbar, SnackbarCloseReason, SnackbarOrigin } from "@mui/material";
-import React from "react";
+import {
+    Alert,
+    AlertColor,
+    Slide,
+    Snackbar,
+    SnackbarCloseReason,
+    SnackbarOrigin,
+} from "@mui/material";
+import React, { SyntheticEvent } from "react";
 
 type ToastProps = {
     msg: string;
     severity: AlertColor;
     open: boolean;
-    onClose: (event?: React.SyntheticEvent | Event, reason?: SnackbarCloseReason) => void;
+    setOpen: (open: boolean) => void;
     autoHideDuration?: number;
     anchorOrigin?: SnackbarOrigin;
 };
 
 /**
- * Toastコンポーネントを返す。デフォルトでは画面右下に表示され、6秒後に消える
+ * Toastコンポーネントを返す。デフォルトでは画面右上に画面右端からスライドして表示され、6秒後に消える
  * @param msg Toastで表示するメッセージ
  * @param severity Toastの重要度
- * @param open 開閉制御用フラグ
- * @param onClose 閉じる際のコールバック
+ * @param open 開閉制御用ステート
+ * @param setOpen 閉じるためのステート更新用関数
  * @param autoHideDuration 自動的に閉じるまでの秒数(ms)
  * @param anchorOrigin 表示するポジション
  */
@@ -23,21 +30,32 @@ const Toast = ({
     msg,
     severity,
     open,
-    onClose,
+    setOpen,
     autoHideDuration = 6000,
-    anchorOrigin = { horizontal: "right", vertical: "bottom" },
+    anchorOrigin = { horizontal: "right", vertical: "top" },
 }: ToastProps) => {
+    const onClose = (event?: SyntheticEvent | Event, reason?: SnackbarCloseReason) => {
+        // トースト以外の領域をクリックしても閉じないよう制御
+        if (reason === "clickaway") {
+            return;
+        }
+        setOpen(false);
+    };
+
     return (
-        <Snackbar
-            open={open}
-            autoHideDuration={autoHideDuration}
-            onClose={onClose}
-            anchorOrigin={anchorOrigin}
-        >
-            <Alert severity={severity} onClose={onClose}>
-                {msg}
-            </Alert>
-        </Snackbar>
+        <Slide in={open} direction={"left"} mountOnEnter unmountOnExit>
+            <Snackbar
+                open={open}
+                autoHideDuration={autoHideDuration}
+                onClose={onClose}
+                anchorOrigin={anchorOrigin}
+                sx={{ paddingTop: "10vh" }}
+            >
+                <Alert severity={severity} onClose={onClose}>
+                    {msg}
+                </Alert>
+            </Snackbar>
+        </Slide>
     );
 };
 
