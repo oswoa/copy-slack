2c9c0726ef24ccba41ef20d33190206e2aff8faf
osw <ows.oao@gmail.com>
2025-10-23 08:25:27 +0900

    FIX: エラーコードを追加

--- src/app/contants/errorMessages.ts
+++ src/app/contants/errorMessages.ts
@@ -1,5 +1,6 @@
 export const ERROR_MESSAGES = {
-    ERROR_UNKNOWN: () => "不明なエラーが発生しました",
+    // クライアントエラー
+    ERROR_CLIENT_UNKNOWN: () => "不明なエラーが発生しました",
     ERROR_VALIDATION_USER_ID_MIN_LENGTH: (length: number) =>
         `ユーザIDは${length}文字以上で入力してください`,
     ERROR_VALIDATION_USER_ID_MAX_LENGTH: (length: number) =>
@@ -10,4 +11,8 @@ export const ERROR_MESSAGES = {
     ERROR_VALIDATION_PASSWORD_MAX_LENGTH: (length: number) =>
         `パスワードは${length}文字以内で入力してください`,
     ERROR_VALIDATION_INCORRECT_EMAIL: () => "不正なメールアドレスです",
+
+    // サーバエラー
+    ERROR_SERVER_UNKNOWN: () => "サーバで不明なエラーが発生しました",
+    ERROR_SERVER_USER_UNAUTHORIZED: () => "ユーザIDもしくはパスワードが間違っています",
 } as const;
