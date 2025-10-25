497f89dbb23ee3296759963d9a152bd62733d337
osw <ows.oao@gmail.com>
2025-10-25 09:27:30 +0900

    FIX: 変数の名前など微修正

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -5,23 +5,30 @@ import { ERROR_CODES } from "@/app/contants/errorCodes";
 import { ERROR_MESSAGES } from "@/app/contants/errorMessages";
 import { HttpStatusCode } from "axios";
 
+type UserResponse = {
+    id: string;
+    email: string;
+    password: string;
+    token: string;
+};
+
 /**
  * ユーザ照会API
  * @param param1 ユーザID
  * @returns ユーザ、エラー情報
  */
 export async function GET(_: Request, { params }: { params: Promise<{ id: string }> }) {
-    let user: User | undefined;
     let status: HttpStatusCode = HttpStatusCode.Ok;
     let errorDetail: ErrorDetail | undefined;
+    let user: User | undefined;
 
     try {
         const { id } = await params;
-        const res = await fetch(`${BASE_URL}/users/${id}`);
-        switch (res.status) {
+        const resData = await fetch(`${BASE_URL}/users/${id}`);
+        switch (resData.status) {
             case HttpStatusCode.Ok:
-                const data: User = await res.json();
-                user = new User(data.id, data.token);
+                const data: UserResponse = await resData.json();
+                user = new User(data.id, data.email, data.token);
                 break;
 
             case HttpStatusCode.NotFound:
