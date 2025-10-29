9162fea28330e5cf60d4e68fcff47fcc07eaa1a1
osw <ows.oao@gmail.com>
2025-10-29 09:57:59 +0900

    エラーコードの追加

--- src/app/contants/errorMessages.ts
+++ src/app/contants/errorMessages.ts
@@ -11,8 +11,10 @@ export const ERROR_MESSAGES = {
     ERROR_VALIDATION_PASSWORD_MAX_LENGTH: (length: number) =>
         `パスワードは${length}文字以内で入力してください`,
     ERROR_VALIDATION_INCORRECT_EMAIL: () => "不正なメールアドレスです",
+    ERROR_CLIENT_WORKSPACE_DOESNT_EXIST: () => "ユーザのワークスペースが存在しません",
 
     // サーバエラー
     ERROR_SERVER_UNKNOWN: () => "サーバで不明なエラーが発生しました",
     ERROR_SERVER_USER_UNAUTHORIZED: () => "ユーザIDもしくはパスワードが間違っています",
+    ERROR_SERVER_WORKSPACES_DOESNT_EXIST: () => "サーバにワークスペースが存在しません",
 } as const;
