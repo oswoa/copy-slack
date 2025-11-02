0583c1485268d2233426683c94b4e2b9076dd75e
osw <ows.oao@gmail.com>
2025-11-02 16:22:06 +0900

    不要なコードを削除

--- src/app/(auth)/workspace/[workspaceId]/[channelId]/components/WorkspaceSwitcher/WorkspaceSwitcher.tsx
+++ src/app/(auth)/workspace/[workspaceId]/[channelId]/components/WorkspaceSwitcher/WorkspaceSwitcher.tsx
@@ -1,24 +1,11 @@
 "use client";
 
-import { CircularProgress } from "@mui/material";
-import { useEffect, useState } from "react";
-import { HttpStatusCode } from "axios";
-
-import { ERROR_CODES } from "@/app/contants/errorCodes";
-import { ERROR_MESSAGES } from "@/app/contants/errorMessages";
-
-import { Workspace } from "@/app/common/Workspace";
-import { ErrorDetail } from "@/app/common/ErrorDetail";
-import Toast from "@/app/common/components/Toast";
-
-import WorkspaceList from "./WorkspaceList";
-
 import { useUserWorkspaces } from "@/app/context/UserWorkspacesContext";
 import { useRouter } from "next/navigation";
 
+import WorkspaceList from "./WorkspaceList";
+
 const WorkspaceSwitcher = () => {
-    // const [toastOpen, setToastOpen] = useState(false);
-    // const [toastErrMsg, setToastErrMsg] = useState("");
     const router = useRouter();
     const workspaces = useUserWorkspaces();
 
