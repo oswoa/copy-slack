6dea14c788d9118979029b68aa4c86a353f5ac3c
osw <ows.oao@gmail.com>
2025-11-27 18:35:57 +0900

    FIX: スキーマを更新

---
+++ prisma/migrations/20251127093237_create_profile_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "profileId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" TEXT NOT NULL,
+    "imageUrl" TEXT,
+    CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("userId") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
