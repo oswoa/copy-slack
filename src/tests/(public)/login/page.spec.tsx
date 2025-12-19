45d2643cf088350e96304fae65acebb2574cd177
osw <ows.oao@gmail.com>
2025-12-19 16:23:08 +0900

    ログイン画面のUTを追加

---
+++ src/tests/(public)/login/page.spec.tsx
@@ -0,0 +1,287 @@
+import { getByRole, render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+import LoginComponent from "@/app/(public)/login/page";
+import { CurrentUserProvider } from "@/app/context/CurrentUserContext";
+import { ToastProvider } from "@/app/context/ToastContext";
+import { mockGetChannelListApi, mockGetWorkspaceListApi, mockLoginApi } from "@/tests/handlers";
+import { mockReplace } from "../../../../vitest.setup";
+
+describe("LoginComponent", () => {
+    const DisplayPage = () => (
+        <CurrentUserProvider>
+            <ToastProvider>
+                <LoginComponent />
+            </ToastProvider>
+        </CurrentUserProvider>
+    );
+
+    describe("正常系", () => {
+        describe("初期表示", () => {
+            it("タイトルが「Copy Slack」であること", async () => {
+                // Arrange
+                render(<DisplayPage />);
+
+                // Act
+                const title = screen.getByRole("heading", { level: 1 });
+
+                // Assert
+                expect(title).toHaveTextContent("Copy Slack");
+            });
+
+            it("「ユーザID」入力テキストボックスが存在すること", async () => {
+                // Arrange
+                render(<DisplayPage />);
+
+                // Act
+                const input = screen.getByLabelText("ユーザID *");
+
+                // Assert
+                expect(input).toBeInTheDocument();
+            });
+
+            it("「パスワード」入力テキストボックスが存在すること", async () => {
+                // Arrange
+                render(<DisplayPage />);
+
+                // Act
+                const input = screen.getByLabelText("パスワード *");
+
+                // Assert
+                expect(input).toBeInTheDocument();
+            });
+
+            it("「ログイン」ボタンが存在すること", async () => {
+                // Arrange
+                render(<DisplayPage />);
+
+                // Act
+                const loginButton = screen.getByRole("button", { name: "ログイン" });
+
+                // Assert
+                expect(loginButton).toBeInTheDocument();
+                expect(loginButton).toBeDisabled();
+            });
+
+            it("ユーザ登録ページへのリンクが設定されていること", async () => {
+                // Arrange
+                render(<DisplayPage />);
+
+                // Act
+                const link = screen.getByRole("link", { name: "こちら" });
+
+                // Assert
+                expect(link).toBeInTheDocument();
+                expect(link).toHaveAttribute("href", "/signup");
+            });
+        });
+    });
+
+    describe("バリデーション", () => {
+        it("ユーザIDに3文字以上の入力でバリデーションエラーが発生しないこと", async () => {
+            // Arrange
+            render(<DisplayPage />);
+            const user = userEvent.setup();
+
+            // Act
+            const input = screen.getByLabelText("ユーザID *");
+            await user.type(input, "a".repeat(3));
+            await user.tab();
+
+            // Assert
+            const validationErrMsg = screen.queryByText("ユーザIDは3文字以上で入力してください");
+            expect(validationErrMsg).not.toBeInTheDocument();
+        });
+
+        it("パスワードに8文字以上の入力でバリデーションエラーが発生しないこと", async () => {
+            // Arrange
+            render(<DisplayPage />);
+            const user = userEvent.setup();
+
+            // Act
+            const input = screen.getByLabelText("パスワード *");
+            await user.type(input, "a".repeat(8));
+            await user.tab();
+
+            // Assert
+            const validationErrMsg = screen.queryByText("パスワードは8文字以上で入力してください");
+            expect(validationErrMsg).not.toBeInTheDocument();
+        });
+
+        it("ユーザID, パスワードでバリデーションエラーが発生しないとき、「ログイン」ボタンが有効であること", async () => {
+            // Arrange
+            render(<DisplayPage />);
+            const user = userEvent.setup();
+
+            // Act
+            const userId = screen.getByLabelText("ユーザID *");
+            const password = screen.getByLabelText("パスワード *");
+            await user.type(userId, "a".repeat(3));
+            await user.type(password, "a".repeat(8));
+            await user.tab();
+
+            // Assert
+            const loginButton = screen.getByRole("button", { name: "ログイン" });
+            expect(loginButton).toBeInTheDocument();
+            expect(loginButton).toBeEnabled();
+        });
+    });
+
+    describe("ボタン制御", () => {
+        it("「ログイン」ボタン押下で指定したAPIが動くこと", async () => {
+            // Arrange
+            render(<DisplayPage />);
+            const user = userEvent.setup();
+            const userId = "user1";
+            const password = "password";
+            const ownerId = userId;
+            const workspaceId = "1";
+
+            // Act
+            const inputUserId = screen.getByLabelText("ユーザID *");
+            const inputPassword = screen.getByLabelText("パスワード *");
+            await user.type(inputUserId, userId);
+            await user.type(inputPassword, password);
+            await user.tab();
+
+            const loginButton = screen.getByRole("button", { name: "ログイン" });
+            await user.click(loginButton);
+
+            // Assert
+            // ログインAPI
+            expect(mockLoginApi).toHaveBeenCalledTimes(1);
+            expect(mockLoginApi).toHaveBeenCalledWith({
+                userId,
+                password,
+            });
+
+            // ワークスペース一覧取得API
+            expect(mockGetWorkspaceListApi).toHaveBeenCalledTimes(1);
+            expect(mockGetWorkspaceListApi).toHaveBeenCalledWith({
+                ownerId,
+            });
+
+            // チャネル一覧取得API
+            expect(mockGetChannelListApi).toHaveBeenCalledTimes(1);
+            expect(mockGetChannelListApi).toHaveBeenCalledWith({
+                workspaceId,
+            });
+        });
+
+        it("「ログイン」ボタン押下でワークスペースへ遷移すること", async () => {
+            // Arrange
+            render(<DisplayPage />);
+            const user = userEvent.setup();
+            const userId = "user1";
+            const password = "password";
+
+            // Act
+            const inputUserId = screen.getByLabelText("ユーザID *");
+            const inputPassword = screen.getByLabelText("パスワード *");
+            await user.type(inputUserId, userId);
+            await user.type(inputPassword, password);
+            await user.tab();
+
+            const loginButton = screen.getByRole("button", { name: "ログイン" });
+            await user.click(loginButton);
+
+            // Assert
+            await waitFor(() => {
+                expect(mockReplace).toHaveBeenCalledTimes(1);
+                expect(mockReplace).toHaveBeenCalledWith("/workspace/1/1");
+            });
+        });
+    });
+
+    // MUIのLinkコンポーネントを利用してるため、初期表示でリンクが表示されていることを確認すれば問題なし
+    // describe("リンク制御", () => {
+    // it("リンク「こちら」からユーザ登録ページへ遷移すること", async () => {});
+    // });
+
+    describe("異常系", () => {
+        describe("バリデーション", () => {
+            describe("ユーザID", () => {
+                it("ユーザIDに3文字未満の入力でバリデーションエラーが発生すること", async () => {
+                    // Arrange
+                    render(<DisplayPage />);
+                    const user = userEvent.setup();
+
+                    // Act
+                    const input = screen.getByLabelText("ユーザID *");
+                    await user.type(input, "a".repeat(2));
+                    await user.tab();
+
+                    // Assert
+                    const validationErrMsg =
+                        screen.queryByText("ユーザIDは3文字以上で入力してください");
+                    expect(validationErrMsg).toBeInTheDocument();
+                });
+
+                it("ユーザIDに3文字未満の入力で「ログイン」ボタンが無効化されていること", async () => {
+                    // Arrange
+                    render(<DisplayPage />);
+                    const user = userEvent.setup();
+
+                    // Act
+                    const input = screen.getByLabelText("ユーザID *");
+                    await user.type(input, "a".repeat(2));
+                    await user.tab();
+
+                    // Assert
+                    const loginButton = screen.getByRole("button", { name: "ログイン" });
+                    expect(loginButton).toBeDisabled();
+                });
+            });
+
+            describe("パスワード", () => {
+                it("パスワードに8文字未満の入力でバリデーションエラーが発生すること", async () => {
+                    // Arrange
+                    render(<DisplayPage />);
+                    const user = userEvent.setup();
+
+                    // Act
+                    const input = screen.getByLabelText("パスワード *");
+                    await user.type(input, "a".repeat(7));
+                    await user.tab();
+
+                    // Assert
+                    const validationErrMsg =
+                        screen.queryByText("パスワードは8文字以上で入力してください");
+                    expect(validationErrMsg).toBeInTheDocument();
+                });
+
+                it("パスワードに8文字未満の入力で「ログイン」ボタンが無効化されていること", async () => {
+                    // Arrange
+                    render(<DisplayPage />);
+                    const user = userEvent.setup();
+
+                    // Act
+                    const input = screen.getByLabelText("パスワード *");
+                    await user.type(input, "a".repeat(7));
+                    await user.tab();
+
+                    // Assert
+                    const loginButton = screen.getByRole("button", { name: "ログイン" });
+                    expect(loginButton).toBeDisabled();
+                });
+            });
+
+            it("ユーザID、パスワードでバリデーションエラーが発生している時、「ログイン」ボタンが無効化されていること", async () => {
+                // Arrange
+                render(<DisplayPage />);
+                const user = userEvent.setup();
+
+                // Act
+                const userId = screen.getByLabelText("ユーザID *");
+                const password = screen.getByLabelText("パスワード *");
+                await user.type(userId, "a".repeat(2));
+                await user.type(password, "a".repeat(7));
+                await user.tab();
+
+                // Assert
+                const loginButton = screen.getByRole("button", { name: "ログイン" });
+                expect(loginButton).toBeDisabled();
+            });
+        });
+    });
+});
--- src/tests/handlers.ts
+++ src/tests/handlers.ts
@@ -1,12 +1,17 @@
+import { LoginApiRequest } from "@/app/api/login/route";
 import { UpdateUserApiRequest } from "@/app/api/users/[userId]/route";
 import { ErrorDetail } from "@/app/common/ErrorDetail";
 import { SafeUser, UserProfile } from "@/app/context/CurrentUserContext";
+import { Channel, Workspace } from "@prisma/client";
 import { HttpStatusCode } from "axios";
 import { http, HttpResponse } from "msw";
 import { vi } from "vitest";
 
 const errorDetail = ErrorDetail.success();
 const status = HttpStatusCode.Ok;
+
+// ログインAPI
+export const mockLoginApi = vi.fn();
 
 // ログアウトAPI
 export const mockLogoutApi = vi.fn();
@@ -20,8 +25,42 @@ export const mockUpdateUserApi = vi.fn();
 // ユーザプロフィール更新API
 export const mockUpdateUserProfileApi = vi.fn();
 
+// チャネル一覧取得API
+export const mockGetChannelListApi = vi.fn();
+
+// ワークスペース一覧取得API
+export const mockGetWorkspaceListApi = vi.fn();
+
 // MSWモック一覧
 export const handlers = [
+    // 認証API
+    http.get("/api/auth", () => {
+        const user: UserProfile = {
+            userId: "user1",
+            displayName: "ユーザー1",
+            email: "test1@example.com",
+        };
+        return HttpResponse.json({ user, errorDetail }, { status });
+    }),
+
+    // ログインAPI
+    http.post<LoginApiRequest>("/api/login", async ({ request }) => {
+        const data = await request.clone().json();
+        const { userId, password } = data as LoginApiRequest;
+
+        const user: UserProfile = {
+            userId: "user1",
+            email: "test1@example.com",
+            displayName: "ユーザ1",
+        };
+
+        mockLoginApi({
+            userId,
+            password,
+        });
+        return HttpResponse.json({ user, errorDetail }, { status });
+    }),
+
     // ログアウトAPI
     http.post("/api/logout", () => {
         mockLogoutApi();
@@ -55,7 +94,7 @@ export const handlers = [
         "/api/users/:userId",
         async ({ params, request }) => {
             const { userId } = params;
-            const data = await request.json();
+            const data = await request.clone().json();
 
             const user: UserProfile = {
                 userId: userId,
@@ -74,7 +113,7 @@ export const handlers = [
     // ユーザプロフィール画像更新API
     http.patch<{ userId: string }>("/api/users/:userId/profile", async ({ params, request }) => {
         const { userId } = params;
-        const data = await request.formData();
+        const data = await request.clone().formData();
         const file = data.get("file") as File;
 
         mockUpdateUserProfileApi({
@@ -83,6 +122,46 @@ export const handlers = [
             type: file.type,
         });
         return HttpResponse.json({ imageUrl: "test.png", errorDetail }, { status });
+    }),
+
+    // チャネル一覧取得API
+    http.get("/api/channels", async ({ request }) => {
+        const url = new URL(request.url);
+        const workspaceId = url.searchParams.get("workspaceId");
+        const channels: Channel[] = [
+            {
+                channelId: 1,
+                workspaceId: "1",
+                channelName: "ch1",
+            },
+            {
+                channelId: 2,
+                workspaceId: "1",
+                channelName: "ch2",
+            },
+        ];
+        mockGetChannelListApi({ workspaceId });
+        return HttpResponse.json({ channels, errorDetail }, { status });
+    }),
+
+    // ワークスペース一覧取得API
+    http.get("/api/workspaces", async ({ request }) => {
+        const url = new URL(request.url);
+        const ownerId = url.searchParams.get("ownerId");
+        const workspaces: Workspace[] = [
+            {
+                workspaceId: "1",
+                ownerId: "user1",
+                workspaceName: "user-wk1",
+            },
+            {
+                workspaceId: "2",
+                ownerId: "user1",
+                workspaceName: "user-wk2",
+            },
+        ];
+        mockGetWorkspaceListApi({ ownerId });
+        return HttpResponse.json({ workspaces, errorDetail }, { status });
     }),
 
     // Socket.io（単体テストには不要だが、ソケット通信自体もモック化しないとエラーがログに溢れるため）
